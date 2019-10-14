6e295386177803fa51c9e9fbea721c64a588c238
Aron Hrafnsson <aronhr98@gmail.com>
2019-10-14 16:49:08 +0000

    Saves checked shortnames and thousand seperator

--- app.py
+++ app.py
@@ -1,24 +1,43 @@
 import urllib.request
 import json
 import PySimpleGUI as sg
+import locale
+locale.setlocale(locale.LC_ALL, 'is_IS')
+'is_IS'
+locale.format_string('%f', 0, grouping=True)
 
-gl = [{'shortName': 'ISK', 'longName': 'Íslensk Króna', 'value': 1, 'askValue': 1, 'bidValue': 1, 'changeCur': 0.22, 'changePer': 0}]   # Preset ISK kr
-shortNames = ['ISK']
+sg.ChangeLookAndFeel('SystemDefault')
+valin_gengi = [{'shortName': 'ISK', 'longName': 'Íslensk Króna', 'value': 1, 'askValue': 1, 'bidValue': 1, 'changeCur': 0.22, 'changePer': 0}]   # Preset ISK kr
+shortNames = {'ISK'}
+
+try:
+    with open('shortNames.txt', 'r') as f:
+        data = f.read()
+        for x in data.split():
+            if x not in shortNames:
+                shortNames.add(x)
+except:
+    pass
 
 with urllib.request.urlopen("https://apis.is/currency/lb") as url:
     gengi = json.loads(url.read())
     gengi = gengi["results"]
 
 
+def addToFile():
+    with open('shortNames.txt', 'w') as f:
+        f.write(' '.join(shortNames))
+
+
 def addToGengi(gengi):
-    if gengi not in gl:
-        gl.append(gengi)
-        shortNames.append(gengi['shortName'])
+    if gengi not in valin_gengi:
+        valin_gengi.append(gengi)
+        shortNames.add(gengi['shortName'])
 
 
 def removeFromGengi(gengi):
-    if gengi in gl:
-        gl.remove(gengi)
+    if gengi in valin_gengi:
+        valin_gengi.remove(gengi)
         shortNames.remove(gengi['shortName'])
 
 
@@ -26,20 +45,21 @@ def l2():
     lay = [[sg.Text('Gjaldmiðill', size=(10, 1)), sg.Text('Kaup', size=(8, 1)), sg.Text('Sala', size=(8, 1)),
                sg.Text('Upphæð', size=(10, 1))]]
     lay += [[sg.Text(x['shortName'], size=(10, 1)), sg.Text('{:.2f}'.format(x['value']), size=(8, 1)),
-             sg.Text('{:.2f}'.format(x['askValue']), size=(8, 1)), sg.Input('{:.2f}'.format(1000 / x['askValue']), size=(10, 1),
-            key=x['shortName'], enable_events=True)] for x in gl]
+             sg.Text('{:.2f}'.format(x['askValue']), size=(8, 1)), sg.Input('{:n}'.format(round(1000 / x['askValue'], 2)), size=(12, 1),
+            key=x['shortName'], enable_events=True)] for x in valin_gengi]
+    lay += [[sg.Button('Baka')]]
     return lay
 
 
 def l1():
     lay = [
-        [sg.Checkbox(x['shortName'], key='_check_' + x['shortName'], size=(5, 1), enable_events=True) for x in
+        [sg.Checkbox(x['shortName'], key='_check_' + x['shortName'], size=(5, 1), enable_events=True, default=True if x['shortName'] in shortNames else False) for x in
          gengi[:5]],
-        [sg.Checkbox(x['shortName'], key='_check_' + x['shortName'], size=(5, 1), enable_events=True) for x in
+        [sg.Checkbox(x['shortName'], key='_check_' + x['shortName'], size=(5, 1), enable_events=True, default=True if x['shortName'] in shortNames else False) for x in
          gengi[5:10]],
-        [sg.Checkbox(x['shortName'], key='_check_' + x['shortName'], size=(5, 1), enable_events=True) for x in
+        [sg.Checkbox(x['shortName'], key='_check_' + x['shortName'], size=(5, 1), enable_events=True, default=True if x['shortName'] in shortNames else False) for x in
          gengi[10:15]],
-        [sg.Checkbox(x['shortName'], key='_check_' + x['shortName'], size=(5, 1), enable_events=True) for x in
+        [sg.Checkbox(x['shortName'], key='_check_' + x['shortName'], size=(5, 1), enable_events=True, default=True if x['shortName'] in shortNames else False) for x in
          gengi[15:20]],
         [sg.Button('Vista')]
     ]
@@ -51,16 +71,16 @@ def calculate(window, org):
         values[org] = float(values[org])
         if values[org] != '' and type(values[org]) is float:
             land = None
-            for x in gl:
+            for x in valin_gengi:
                 if x['shortName'] == org:
                     land = x
                     break
             new_isk = float(values[org]) * land['askValue']
-            for x in gl:
+            for x in valin_gengi:
                 if x['shortName'] == org:
                     continue
                 calc = new_isk / float(x['askValue'])
-                window.Element(x['shortName']).Update('{:.2f}'.format(calc))
+                window.Element(x['shortName']).Update('{:n}'.format(round(calc, 2)))
     except:
         pass
 
@@ -75,6 +95,7 @@ while True:
     event, values = window.read()
     if event in (None, 'Cancel'):  # if user closes window or clicks cancel
         break
+
     if windowName == 'Gengi':
         for x in gengi:
             check = window.FindElement('_check_' + x['shortName'])
@@ -82,6 +103,14 @@ while True:
                 check.Update(addToGengi(x))
             else:
                 check.Update(removeFromGengi(x))
+            addToFile()
+    print(shortNames)
+    if event == 'Baka':
+        windowName = 'Gengi'
+        layout = l1()
+        window1 = sg.Window(windowName, layout, keep_on_top=True)
+        window.close()
+        window = window1
     if event == 'Vista':
         windowName = 'Gengi reiknivél'
         layout = l2()
